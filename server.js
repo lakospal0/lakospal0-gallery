e87106708cc4fbaa9c2e47395f4b30b9f7285275
lakospal0 <lakospal0@gmail.com>
2019-10-25 23:06:44 +0200

    css and html are upgraded

--- server.js
+++ server.js
@@ -5,8 +5,25 @@ const app = express();
 app.use(express.static('static'));
 app.set('view engine', 'ejs');
 
+const path = require('path');
+const fs = require('fs');
+
+const directoryPath = path.join(__dirname, 'pictures');
+
+fs.readdir(directoryPath, function(err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+    //listing all files using forEach
+    files.forEach(function(file) {
+        // Do whatever you want to do with the file
+        console.log(file);
+    });
+});
+
 app.get('/', function(req, res) {
-    res.render('home.ejs');
+    res.render('home.ejs', { person: "Guest" });
 })
 
 app.listen(PORT);

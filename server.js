721a6088d12ba4346747c8e83a476181b106ca93
lakospal0 <lakospal0@gmail.com>
2019-10-25 14:23:37 +0200

    made some html and css progression

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const PORT = 3000;
 const app = express();
 
+app.use(express.static('static'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {

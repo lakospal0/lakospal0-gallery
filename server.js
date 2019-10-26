77f12439c8f2e3c0afea87074cde96353a9b9ae9
lakospal0 <lakospal0@gmail.com>
2019-10-26 20:57:59 +0200

    overpowered thingy

--- server.js
+++ server.js
@@ -5,25 +5,15 @@ const app = express();
 app.use(express.static('static'));
 app.set('view engine', 'ejs');
 
-const path = require('path');
+
 const fs = require('fs');
 
-const directoryPath = path.join(__dirname, 'pictures');
-
-fs.readdir(directoryPath, function(err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    }
-    //listing all files using forEach
-    files.forEach(function(file) {
-        // Do whatever you want to do with the file
-        console.log(file);
-    });
-});
+let rawdata = fs.readFileSync('data.json');
+let images = JSON.parse(rawdata);
+let size = Object.keys(images).length;
 
 app.get('/', function(req, res) {
-    res.render('home.ejs', { person: "Guest" });
+    res.render('home.ejs', { pictures: images, size: size });
 })
 
 app.listen(PORT);
